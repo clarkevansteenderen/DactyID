d91fc4a8d9cd3e674834ccd4fb382269a7408267
Clarke <vsteenderen@gmail.com>
2020-02-13 09:13:09 +0200

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,3 +1,9 @@
+mypackages <- c("shiny", "shinyhelper", "magrittr", "DECIPHER", "seqinr", "shinyFiles", "shinythemes", "dendextend")
+checkpkg <- mypackages[!(mypackages %in% installed.packages()[,"Package"])]
+if(length(checkpkg)) install.packages(checkpkg, dependencies = TRUE)
+
+
+
 library(shiny)
 library(DECIPHER)
 library(seqinr)
@@ -7,19 +13,19 @@ library(magrittr) # allows you to use %>%
 library(shinythemes)
 
 ui <- fluidPage(theme = shinytheme("flatly"),
-                
+
   titlePanel(strong(h1("DACTY-ID"))),
   titlePanel(h3("For the genetic identification of the Dactylopiidae")),
-  img(src='logo.png',  height = '150px', width = '200px'),   
+  img(src='logo.png',  height = '150px', width = '200px'),
   titlePanel(h4("Created by Clarke van Steenderen")),
-  br(), 
-  
+  br(),
+
   tabsetPanel(tabPanel("Home",
   #sidebarLayout(
     sidebarPanel(strong("Click to view the help file:", style = "color:darkgreen")
                  %>% helper(type = "markdown", content = "DactylopiusIdentifier_Manual", colour = "green", icon = "question-circle"),
                  br(),
-                 fileInput("inFile", "Upload a .fas file"), 
+                 fileInput("inFile", "Upload a .fas file"),
                  # list.files("Gene_databases/") is the name of the folder in this project's file (Dactylopius Identifier R/ DactylopiusID) which contains .fas files
                  selectInput("selectfile","Select File", choice = c(list.files("Gene_databases/"), "Own file upload"), selected = "Own file upload" ),
                  textInput("name", label = "Enter sequence name:", value = ">QUERY"),
@@ -29,9 +35,9 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                  actionButton("submit", label = "Submit", style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
                  ),
   mainPanel() ),
-  
+
   tabPanel("View Tree", plotOutput("treePlot", height = "1600px"))
-  
+
 )
   )
 
